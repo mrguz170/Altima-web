e0b987e2d99f85629a84f318085619eab66d2421
SxVx <drilosv13@gmail.com>
2019-07-17 15:46:47 -0500

    Visualizacion de registro
    En las secciones donde el usuario esta registrado, se cambia el texto de Registrarse a Registrado en color rojo, mayor cambio significativo en el Home.js

--- Home.js
+++ Home.js
@@ -10,13 +10,71 @@ window.onload = function() {
     }
 
 
-    document.getElementById("ws1_1").onclick = function(){ registro("ws1"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; };
-    document.getElementById("ws1_2").onclick = function(){ registro("ws1"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; };
-    document.getElementById("ws2_1").onclick = function(){ registro("ws2"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; };
-    document.getElementById("ws2_2").onclick = function(){ registro("ws2"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; };
+    document.getElementById("ws1_1").onclick = function(){ registro("ws1"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; 
+                document.getElementById("ws1_2").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; document.getElementById("ws1_2").disabled = true;
+    };
+    document.getElementById("ws1_2").onclick = function(){ registro("ws1"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; 
+        document.getElementById("ws1_1").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; document.getElementById("ws1_1").disabled = true;
+    };
+    
+    document.getElementById("ws2_1").onclick = function(){ registro("ws2"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; 
+        document.getElementById("ws2_2").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; document.getElementById("ws2_2").disabled = true;
+    };
+    document.getElementById("ws2_2").onclick = function(){ registro("ws2"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; 
+        document.getElementById("ws2_1").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; document.getElementById("ws2_1").disabled = true;
+    };
     document.getElementById("sk1").onclick = function(){ registro("sk1"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; };
     document.getElementById("sk2").onclick = function(){ registro("sk2"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; };
     document.getElementById("sk3").onclick = function(){ registro("sk3"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; };
+
+    firebase.database().ref('WorkShopsSubcriptions/' + firebase.auth().currentUser.uid)
+    .on("child_added", function(snapshot) {
+        
+        switch (snapshot.key) {
+            case 'one':
+                    if(snapshot.val()){
+                        document.getElementById("ws1_1").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>';
+                        document.getElementById("ws1_2").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>';
+                        document.getElementById("ws1_1").disabled = true;
+                        document.getElementById("ws1_2").disabled = true;
+                    }
+                break;
+            case 'two':
+                if(snapshot.val()){
+                    document.getElementById("ws2_1").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>';
+                    document.getElementById("ws2_2").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>';
+                    document.getElementById("ws2_2").disabled = true;
+                    document.getElementById("ws2_1").disabled = true;
+                }
+                break;
+        }
+
+    });
+
+    firebase.database().ref('SpeakersSubcriptions/' + firebase.auth().currentUser.uid)
+    .on("child_added", function(snapshot) {
+        switch (snapshot.key) {
+            case 'one':
+                if(snapshot.val()){
+                    document.getElementById("sk1").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; 
+                    document.getElementById("sk1").disabled = true;
+                }
+                break;
+            case 'two':
+                if(snapshot.val()){
+                    document.getElementById("sk2").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; 
+                    document.getElementById("sk2").disabled = true;
+                }
+                break;
+            case 'three':
+                if(snapshot.val()){
+                    document.getElementById("sk3").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; 
+                    document.getElementById("sk3").disabled = true;
+                }
+                break;
+        }
+    });
+
 
 };
 
