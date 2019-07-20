3a04b861d6fef8b22bfc0ee7535503f3dbf9459c
SxVx <drilosv13@gmail.com>
2019-07-20 18:39:42 -0500

    Nuevos registros agregados
    Se agregaron registro a las nuevas conferencias, en total 7 workshop y 3 speaker, se agregaron los botones de registro en el home.html y su funcionalidad en Home.js

--- Home.js
+++ Home.js
@@ -10,22 +10,17 @@ window.onload = function() {
     }
 
 
-    document.getElementById("ws1_1").onclick = function(){ registro("ws1"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; 
-                document.getElementById("ws1_2").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; document.getElementById("ws1_2").disabled = true;
-    };
-    document.getElementById("ws1_2").onclick = function(){ registro("ws1"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; 
-        document.getElementById("ws1_1").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; document.getElementById("ws1_1").disabled = true;
-    };
-    
-    document.getElementById("ws2_1").onclick = function(){ registro("ws2"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; 
-        document.getElementById("ws2_2").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; document.getElementById("ws2_2").disabled = true;
-    };
-    document.getElementById("ws2_2").onclick = function(){ registro("ws2"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; 
-        document.getElementById("ws2_1").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; document.getElementById("ws2_1").disabled = true;
-    };
+    document.getElementById("ws1").onclick = function(){ registro("ws1"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>';this.disabled = true;};
+    document.getElementById("ws2").onclick = function(){ registro("ws2"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true;};
+    document.getElementById("ws3").onclick = function(){ registro("ws3"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; };
+    document.getElementById("ws4").onclick = function(){ registro("ws4"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true;};
+    document.getElementById("ws5").onclick = function(){ registro("ws5"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true;};
+    document.getElementById("ws6").onclick = function(){ registro("ws6"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true;};
+    document.getElementById("ws7").onclick = function(){ registro("ws7"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true;};
     document.getElementById("sk1").onclick = function(){ registro("sk1"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; };
     document.getElementById("sk2").onclick = function(){ registro("sk2"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; };
     document.getElementById("sk3").onclick = function(){ registro("sk3"); this.innerHTML='<p style="color:#F00;"><b>Registrado</b></p>'; this.disabled = true; };
+
 
     firebase.database().ref('WorkShopsSubcriptions/' + firebase.auth().currentUser.uid)
     .on("child_added", function(snapshot) {
@@ -33,18 +28,44 @@ window.onload = function() {
         switch (snapshot.key) {
             case 'one':
                     if(snapshot.val()){
-                        document.getElementById("ws1_1").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>';
-                        document.getElementById("ws1_2").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>';
-                        document.getElementById("ws1_1").disabled = true;
-                        document.getElementById("ws1_2").disabled = true;
+                        document.getElementById("ws1").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>';
+                        document.getElementById("ws1").disabled = true;
                     }
                 break;
             case 'two':
                 if(snapshot.val()){
-                    document.getElementById("ws2_1").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>';
-                    document.getElementById("ws2_2").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>';
-                    document.getElementById("ws2_2").disabled = true;
-                    document.getElementById("ws2_1").disabled = true;
+                    document.getElementById("ws2").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>';
+                    document.getElementById("ws2").disabled = true;
+                }
+                break;
+                case 'three':
+                    if(snapshot.val()){
+                        document.getElementById("ws3").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>';
+                        document.getElementById("ws3").disabled = true;
+                    }
+                break;
+            case 'four':
+                if(snapshot.val()){
+                    document.getElementById("ws4").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>';
+                    document.getElementById("ws4").disabled = true;
+                }
+                break;
+                case 'five':
+                    if(snapshot.val()){
+                        document.getElementById("ws5").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>';
+                        document.getElementById("ws5").disabled = true;
+                    }
+                break;
+            case 'six':
+                if(snapshot.val()){
+                    document.getElementById("ws6").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>';
+                    document.getElementById("ws6").disabled = true;
+                }
+                break;
+            case 'seven':
+                if(snapshot.val()){
+                    document.getElementById("ws7").innerHTML='<p style="color:#F00;"><b>Registrado</b></p>';
+                    document.getElementById("ws7").disabled = true;
                 }
                 break;
         }
@@ -113,6 +134,46 @@ function registro(x){
                 alert('Registro Exitoso');
             }
             break;
+        case 'ws3':
+            if( confirm("¿Desea hacer registro?") ){
+                firebase.database().ref('WorkShopsSubcriptions/' + firebase.auth().currentUser.uid).update({
+                    three : true
+                });
+                alert('Registro Exitoso');
+            }
+            break;
+        case 'ws4':
+            if( confirm("¿Desea hacer registro?") ){
+                firebase.database().ref('WorkShopsSubcriptions/' + firebase.auth().currentUser.uid).update({
+                    four : true
+                });
+                alert('Registro Exitoso');
+            }
+            break;
+        case 'ws5':
+            if( confirm("¿Desea hacer registro?") ){
+                firebase.database().ref('WorkShopsSubcriptions/' + firebase.auth().currentUser.uid).update({
+                    five : true
+                });
+                alert('Registro Exitoso');
+            }
+            break;
+        case 'ws6':
+            if( confirm("¿Desea hacer registro?") ){
+                firebase.database().ref('WorkShopsSubcriptions/' + firebase.auth().currentUser.uid).update({
+                    six : true
+                });
+                alert('Registro Exitoso');
+            }
+            break;
+        case 'ws7':
+            if( confirm("¿Desea hacer registro?") ){
+                firebase.database().ref('WorkShopsSubcriptions/' + firebase.auth().currentUser.uid).update({
+                    seven : true
+                });
+                alert('Registro Exitoso');
+            }
+            break;
         case 'sk1':
             if( confirm("¿Desea hacer registro?") ){
                 firebase.database().ref('SpeakersSubcriptions/' + firebase.auth().currentUser.uid).update({
@@ -136,7 +197,6 @@ function registro(x){
                 });
                 alert('Registro Exitoso');
             }
-            
             break;
     }
 
